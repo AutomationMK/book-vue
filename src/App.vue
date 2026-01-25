3fb5c283dd57e210c87a34453979094a6dd1c226
Max Kranker <maxkranker@protonmail.com>
2026-01-25 16:03:54 +0700

    Add all components into main template

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,21 @@
 <template>
-    <h1>My app</h1>
+    <App_Header />
+    <App_Body />
+    <App_Footer />
 </template>
 
 <script>
-    export default {
+    import App_Header from "./components/App_Header.vue"
+    import App_Body from "./components/App_Body.vue"
+    import App_Footer from "./components/App_Footer.vue"
 
+    export default {
+        name: 'App',
+        components: {
+            App_Header,
+            App_Body,
+            App_Footer,
+        }
     }
 </script>
 

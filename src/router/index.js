c2daebbd6d280192748f95005b3177eaad2f52ff
Max Kranker <maxkranker@protonmail.com>
2026-01-25 16:14:28 +0700

    Add initial vue-router implementation

---
+++ src/router/index.js
@@ -0,0 +1,13 @@
+import { createRouter, createWebHistory } from "vue-router";
+import App_Body from "./../components/App_Body.vue";
+
+const routes = [
+    {
+        path: "/",
+        name: "Home",
+        component: Body,
+    },
+];
+
+const router = createRouter({ history: createWebHistory(), routes });
+export default router;

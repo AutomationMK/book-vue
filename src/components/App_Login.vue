b8d727e23e1414a359a98298bfa5a40c7c795133
Max Kranker <maxkranker@protonmail.com>
2026-01-25 19:01:34 +0700

    Remove mounted form validation

--- src/components/App_Login.vue
+++ src/components/App_Login.vue
@@ -39,23 +39,5 @@
                 console.log("submitHandler called - success!");
             }
         },
-        mounted() {
-            (function () {
-                'use strict';
-
-                var forms = document.querySelectorAll('.needs-validation');
-                Array.prototype.slice.call(forms)
-                    .forEach(function (form) {
-                        form.addEventListener('submit', function (event) {
-                            if (!form.checkValidity()) {
-                                event.preventDefault();
-                                event.stopPropagation();
-                            }
-
-                            form.classList.add('was-validated');
-                        }, false)
-                    })
-            })();
-        },
     }
 </script>

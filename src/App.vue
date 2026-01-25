a82f3e4dd571b7d3b1a1bbc91210ed903b6d72c1
Max Kranker <maxkranker@protonmail.com>
2026-01-25 16:19:53 +0700

    Implement home route in main application

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,19 @@
 <template>
     <App_Header />
-    <App_Body />
+    <div>
+        <router-view />
+    </div>
     <App_Footer />
 </template>
 
 <script>
     import App_Header from "./components/App_Header.vue"
-    import App_Body from "./components/App_Body.vue"
     import App_Footer from "./components/App_Footer.vue"
 
     export default {
         name: 'App',
         components: {
             App_Header,
-            App_Body,
             App_Footer,
         }
     }

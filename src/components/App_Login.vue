88af9c568490972f19b5b00a2dd3628c5b9a6ea1
Max Kranker <maxkranker@protonmail.com>
2026-01-25 17:55:48 +0700

    Implement login with form and input components

--- src/components/App_Login.vue
+++ src/components/App_Login.vue
@@ -5,9 +5,44 @@
                 <h1 class="mt-5">Login</h1>
                 <hr>
 
-                <hr>
-                <input type="submit" class="btn btn-primary" value="Login">
+                <form method="post" action="/login" class="needs-validation" novalidate>
+                    <text-input label="email" type="email" name="email" required="true"></text-input>
+
+                    <text-input label="password" type="password" name="password" required="true"></text-input>
+
+                    <hr>
+                    <input type="submit" class="btn btn-primary" value="Login">
+                </form>
             </div>
         </div>
     </div>
 </template>
+
+<script>
+    import TextInput from './forms/App_TextInput.vue'
+
+    export default {
+        name: 'App_Login',
+        components: {
+            TextInput,
+        },
+        mounted() {
+            (function () {
+                'use strict'
+
+                var forms = document.querySelectorAll('.needs-validation');
+                Array.prototype.slice.call(forms)
+                    .forEach(function (form) {
+                        form.addEventListener('submit', function (event) {
+                            if (!form.checkValidity()) {
+                                event.preventDefault();
+                                event.stopPropagation();
+                            }
+
+                            form.classList.add('was-validated');
+                        }, false)
+                    })
+            })();
+        },
+    }
+</script>

c270f837d7a47fa7805d8cb3be7e790cfa3fde89
Max Kranker <maxkranker@protonmail.com>
2026-01-25 18:10:00 +0700

    Add FormTag form component

---
+++ src/components/forms/FormTag.vue
@@ -0,0 +1,30 @@
+<template>
+    <form autocomplete="off" :method="method" :action="action" class="needs-validation" novalidate>
+        <slot></slot>
+    </form>
+</template>
+
+<script>
+    export default {
+        name: 'FormTag',
+        props: ["method", "action"],
+        mounted() {
+            (function () {
+                'use strict'
+
+                var forms = document.querySelectorAll('.needs-validation');
+                Array.prototype.slice.call(forms)
+                    .forEach(function (form) {
+                        form.addEventListener('submit', function (event) {
+                            if (!form.checkValidity()) {
+                                event.preventDefault();
+                                event.stopPropagation();
+                            }
+
+                            form.classList.add('was-validated');
+                        }, false)
+                    })
+            })();
+        }
+    }
+</script>

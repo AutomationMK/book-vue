7a7a7f5188ad034ac72cd4ce4ff0f14490309458
Max Kranker <maxkranker@protonmail.com>
2026-01-25 16:22:55 +0700

    Add initial login route

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,17 @@
 import { createRouter, createWebHistory } from "vue-router";
 import App_Body from "./../components/App_Body.vue";
+import App_Login from "./../components/App_Login.vue";
 
 const routes = [
     {
         path: "/",
         name: "Home",
         component: App_Body,
+    },
+    {
+        path: "/login",
+        name: "Login",
+        component: App_Login,
     },
 ];
 

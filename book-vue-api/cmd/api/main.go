0eccb60b3308b7de8a91f0c139f5cfda55154f7f
Max Kranker <maxkranker@protonmail.com>
2026-01-28 22:09:07 +0700

    Add serve funtion that tests passing of json data

--- book-vue-api/cmd/api/main.go
+++ book-vue-api/cmd/api/main.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
 	"log"
+	"net/http"
 	"os"
 )
 
@@ -27,4 +30,33 @@ func main() {
 		infoLog:  infoLog,
 		errorLog: errorLog,
 	}
+
+	err := app.serv()
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func (app *application) serv() error {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		var payload struct {
+			Okay    bool   `json:"okay"`
+			Message string `json:"message"`
+		}
+
+		payload.Okay = true
+		payload.Message = "Hello, world"
+
+		out, err := json.MarshalIndent(payload, "", "\t")
+		if err != nil {
+			app.errorLog.Println(err)
+		}
+
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK)
+		w.Write(out)
+	})
+
+	app.infoLog.Println("API listening on pont", app.config.port)
+	return http.ListenAndServe(fmt.Sprintf(":%d", app.config.port), nil)
 }

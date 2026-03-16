6881a303eff7a97baa2e9a40bbc1dcf38e3241cf
Max Kranker <maxkranker@protonmail.com>
2026-03-16 19:10:58 -0400

    Add notie error message for login form

--- vue/src/components/App_Login.vue
+++ vue/src/components/App_Login.vue
@@ -22,6 +22,8 @@
     import FormTag from './forms/FormTag.vue';
     import TextInput from './forms/TextInput.vue';
     import {store} from './store.js';
+    import router from './../router/index.js'
+    import notie from 'notie'
 
     export default {
         name: 'App_Login',
@@ -55,9 +57,16 @@
                     .then((response) => {
                         if (response.error) {
                             console.log("Error:", response.message);
+                            notie.alert({
+                                type: 'error',
+                                text: response.message,
+                                // stay: true
+                                // position: 'bottom',
+                            });
                         } else {
                             console.log("Token:", response.data.token.token);
                             store.token = response.data.token.token;
+                            router.push("/");
                         }
                     })
             }

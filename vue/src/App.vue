980d1ef88ad1c6eedb55f1a8990967a1140e54ce
Max Kranker <maxkranker@protonmail.com>
2026-03-16 18:58:44 -0400

    Get access to store var

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -9,12 +9,18 @@
 <script>
     import App_Header from "./components/App_Header.vue"
     import App_Footer from "./components/App_Footer.vue"
+    import {store} from './components/store.js';
 
     export default {
         name: 'App',
         components: {
             App_Header,
             App_Footer,
+        },
+        data() {
+            return {
+                store
+            }
         }
     }
 </script>

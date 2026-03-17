622aa01a5a0ebac7aaba103c33f8349f1c7f7af9
Max Kranker <maxkranker@protonmail.com>
2026-03-17 08:52:36 -0400

    Add user frontend templates and routes

--- vue/src/router/index.js
+++ vue/src/router/index.js
@@ -5,6 +5,8 @@ import App_Books from "./../components/App_Books.vue";
 import App_Book from "./../components/App_Book.vue";
 import App_Books_Admin from "./../components/App_Books_Admin.vue";
 import App_Book_Edit from "./../components/App_Book_Edit.vue";
+import App_Users from "./../components/App_Users.vue";
+import App_User_Edit from "./../components/App_User_Edit.vue";
 
 const routes = [
     {
@@ -36,6 +38,16 @@ const routes = [
         path: "/admin/books/:bookId",
         name: "Book_Edit",
         component: App_Book_Edit,
+    },
+    {
+        path: "/admin/users",
+        name: "Users",
+        component: App_Users,
+    },
+    {
+        path: "/admin/users/:userId",
+        name: "User_Edit",
+        component: App_User_Edit,
     },
 ];
 

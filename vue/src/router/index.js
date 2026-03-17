8e3b2801d33adfd6856baf77b45ab4505eca459f
Max Kranker <maxkranker@protonmail.com>
2026-03-17 08:46:42 -0400

    Add book frontend templates and routes

--- vue/src/router/index.js
+++ vue/src/router/index.js
@@ -1,6 +1,10 @@
 import { createRouter, createWebHistory } from "vue-router";
 import App_Body from "./../components/App_Body.vue";
 import App_Login from "./../components/App_Login.vue";
+import App_Books from "./../components/App_Books.vue";
+import App_Book from "./../components/App_Book.vue";
+import App_Books_Admin from "./../components/App_Books_Admin.vue";
+import App_Book_Edit from "./../components/App_Book_Edit.vue";
 
 const routes = [
     {
@@ -12,6 +16,26 @@ const routes = [
         path: "/login",
         name: "Login",
         component: App_Login,
+    },
+    {
+        path: "/books",
+        name: "Books",
+        component: App_Books,
+    },
+    {
+        path: "/books/:bookName",
+        name: "Book",
+        component: App_Book,
+    },
+    {
+        path: "/admin/books",
+        name: "Books_Admin",
+        component: App_Books_Admin,
+    },
+    {
+        path: "/admin/books/:bookId",
+        name: "Book_Edit",
+        component: App_Book_Edit,
     },
 ];
 

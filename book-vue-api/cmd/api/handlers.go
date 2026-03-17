20ef696853a396183b0ab83a09b9c704992a10cd
Max Kranker <maxkranker@protonmail.com>
2026-03-17 14:55:46 -0400

    Add DeleteUser handler

--- book-vue-api/cmd/api/handlers.go
+++ book-vue-api/cmd/api/handlers.go
@@ -189,3 +189,29 @@ func (app *application) GetUser(w http.ResponseWriter, r *http.Request) {
 
 	_ = app.writeJSON(w, http.StatusOK, user)
 }
+
+// DeleteUser deletes a user from the database from json post id
+func (app *application) DeleteUser(w http.ResponseWriter, r *http.Request) {
+	var requestPayload struct {
+		ID int `json:"id"`
+	}
+
+	err := app.readJSON(w, r, &requestPayload)
+	if err != nil {
+		app.errorJSON(w, err)
+		return
+	}
+
+	err = app.models.User.DeleteById(requestPayload.ID)
+	if err != nil {
+		app.errorJSON(w, err)
+		return
+	}
+
+	payload := jsonResponse{
+		Error:   false,
+		Message: "User deleted",
+	}
+
+	_ = app.writeJSON(w, http.StatusOK, payload)
+}

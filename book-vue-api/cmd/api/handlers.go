dcc5ba9cecade7de4cb0f3863d8010af90d771f7
Max Kranker <maxkranker@protonmail.com>
2026-03-19 10:15:03 -0400

    Add ValidateToken handler

--- book-vue-api/cmd/api/handlers.go
+++ book-vue-api/cmd/api/handlers.go
@@ -258,3 +258,26 @@ func (app *application) LogUserOutAndSetInactive(w http.ResponseWriter, r *http.
 
 	_ = app.writeJSON(w, http.StatusAccepted, payload)
 }
+
+// ValidateToken checks if the request token is valid and returns true or false
+func (app *application) ValidateToken(w http.ResponseWriter, r *http.Request) {
+	var requestPayload struct {
+		Token string `json:"token"`
+	}
+
+	err := app.readJSON(w, r, &requestPayload)
+	if err != nil {
+		app.errorJSON(w, err)
+		return
+	}
+
+	valid := false
+	valid, _ = app.models.Token.ValidToken(requestPayload.Token)
+
+	payload := jsonResponse{
+		Error: false,
+		Data:  valid,
+	}
+
+	_ = app.writeJSON(w, http.StatusOK, payload)
+}
